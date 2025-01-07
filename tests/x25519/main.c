bc4113a39d804d4d879cc2067899b672916b5bb6
SH1E0r1r2y <sherry12312337@gmail.com>
2025-01-07 15:55:51 +0800

    vars

--- tests/x25519/main.c
+++ tests/x25519/main.c
@@ -15,21 +15,16 @@
 
 
 /* Assembly Functions */
-/* Reference, bit interlaved */
+/* Reference */
 extern void fe25519_add_wrap(uint32_t *out, uint32_t *a, uint32_t *b);
 extern void fe25519_sub_wrap(uint32_t *out, uint32_t *a, uint32_t *b);
 extern void fe25519_mul_wrap(uint32_t *out, uint32_t *a, uint32_t *b);
-extern void fe25519_sqr_wrap(uint32_t *out, uint32_t *a, uint32_t *b);
-/* To be tested, bit interlaved */
-// extern void KeccakF1600_StatePermute_old(void*);
-// /* M7 optimized code by Alexandre Adomnicai */
-// extern void KeccakP1600_Permute_24rounds(void*);
-
+extern void fe25519_sqr_wrap(uint32_t *out);
 /* Slothy */
 extern void fe25519_add_opt_m7_wrap(uint32_t *out, uint32_t *a, uint32_t *b);
 extern void fe25519_sub_opt_m7_wrap(uint32_t *out, uint32_t *a, uint32_t *b);
 extern void fe25519_mul_opt_m7_wrap(uint32_t *out, uint32_t *a, uint32_t *b);
-extern void fe25519_sqr_opt_m7_wrap(uint32_t *out, uint32_t *a, uint32_t *b);
+extern void fe25519_sqr_opt_m7_wrap(uint32_t *out);
 
 typedef struct {
     char name[100];
@@ -39,20 +34,13 @@ typedef struct {
 benchmark_result results[100];
 int benchmark_cnt = 0;
 
-// static void add_benchmark_results(char *name, uint64_t cycles){
-//     if(benchmark_cnt == 100) return;
+static void add_benchmark_results(char *name, uint64_t cycles){
+    if(benchmark_cnt == 100) return;
 
-//     results[benchmark_cnt].cycles = cycles;
-//     strncpy(results[benchmark_cnt].name, name, 100);
-//     benchmark_cnt++;
-// }
-
-// static void dump_benchmarks_tex(void){
-//     for(int i=0;i<benchmark_cnt;i++){
-
-//         debug_printf("\\DefineVar{%s_%s}{%llu}", DEVICE, results[i].name, results[i].cycles);
-//     }
-// }
+    results[benchmark_cnt].cycles = cycles;
+    strncpy(results[benchmark_cnt].name, name, 100);
+    benchmark_cnt++;
+}
 
 static int cmp_uint32_t(const void *p1, const void *p2)
 {
@@ -82,51 +70,145 @@ static int cmp_uint32_t(const void *p1, const void *p2)
                                                                          \
         }                                                                \
                                                                          \
-        qsort(cycles, REPEAT_MEDIAN, sizeof(uint32_t), cmp_uint32_t);    \
-        debug_printf("%s repeat %d, %d cycles\n", #func,                 \
-                     REPEAT * REPEAT_MEDIAN,                             \
-                     cycles[REPEAT_MEDIAN / 2]);                         \
-                                                                         \
-        return 0;                                                        \
+        qsort(cycles, REPEAT_MEDIAN, sizeof(uint32_t), cmp_uint32_t);      \
+        debug_printf("%s repeat %d, %d cycles\n", #func,                   \
+                     REPEAT * REPEAT_MEDIAN,                               \
+                     cycles[REPEAT_MEDIAN / 2]);                           \
+        add_benchmark_results(#var, (cycles[REPEAT_MEDIAN >> 1]));         \
+        return 0;                                                          \
     }
-
+#define MAKE_BENCH_X25519_single(var, func)                          \
+    int bench_x25519_##var()                                             \
+    {                                                                    \
+        debug_printf("Benchmarking function %s\n", #func);               \
+        uint32_t t1, t2;                                                 \
+        uint32_t cycles[REPEAT_MEDIAN];                                  \
+        uint32_t out[X25519_SIZE] __attribute__((aligned(16))) = {0};    \
+                                                                         \
+        /* Warm up */                                                    \
+        (func)(out);                                                     \
+                                                                         \
+        for (size_t cnt_median = 0; cnt_median < REPEAT_MEDIAN; cnt_median++) { \
+            t1 = hal_get_time();                                         \
+            for (size_t cnt = 0; cnt < REPEAT; cnt++) {                  \
+                (func)(out);                                         \
+            }                                                            \
+            t2 = hal_get_time();                                         \
+            cycles[cnt_median] = (t2 - t1) / REPEAT;                     \
+        }                                                                \
+                                                                         \
+        qsort(cycles, REPEAT_MEDIAN, sizeof(uint32_t), cmp_uint32_t);      \
+        debug_printf("%s repeat %d, %d cycles\n", #func,                   \
+                     REPEAT * REPEAT_MEDIAN,                               \
+                     cycles[REPEAT_MEDIAN / 2]);                           \
+        add_benchmark_results(#var, (cycles[REPEAT_MEDIAN >> 1]));         \
+        return 0;                                                          \
+    }
 
 MAKE_BENCH_X25519(fe25519_add, fe25519_add_wrap)
 MAKE_BENCH_X25519(fe25519_sub, fe25519_sub_wrap)
 MAKE_BENCH_X25519(fe25519_mul, fe25519_mul_wrap)
-MAKE_BENCH_X25519(fe25519_sqr, fe25519_sqr_wrap)
+MAKE_BENCH_X25519_single(fe25519_sqr, fe25519_sqr_wrap)
 
 
 MAKE_BENCH_X25519(fe25519_add_opt_m7,fe25519_add_opt_m7_wrap)
 MAKE_BENCH_X25519(fe25519_sub_opt_m7,fe25519_sub_opt_m7_wrap)
 MAKE_BENCH_X25519(fe25519_mul_opt_m7,fe25519_mul_opt_m7_wrap)
-MAKE_BENCH_X25519(fe25519_sqr_opt_m7,fe25519_sqr_opt_m7_wrap)
+MAKE_BENCH_X25519_single(fe25519_sqr_opt_m7,fe25519_sqr_opt_m7_wrap)
 
+#define MAKE_TEST_X25519(var, func, ref_func)                         \
+    int test_x25519_##var()                                           \
+    {                                                                 \
+        debug_printf("25519 Correctness Test for " #func);            \
+        uint32_t out_ref[X25519_SIZE] __attribute__((aligned(16))) = {0}; \
+        uint32_t out_test[X25519_SIZE] __attribute__((aligned(16))) = {0}; \
+        uint32_t a[X25519_SIZE] __attribute__((aligned(16))) = {0};   \
+        uint32_t b[X25519_SIZE] __attribute__((aligned(16))) = {0};   \
+                                                                      \
+        /* Initialize inputs */                                       \
+        for (size_t i = 0; i < X25519_SIZE; i++) {                    \
+            a[i] = rand();                                            \
+            b[i] = rand();                                            \
+        }                                                             \
+                                                                      \
+        /* Reference function */                                      \
+        ref_func(out_ref, a, b);                                      \
+        /* Test function */                                           \
+        func(out_test, a, b);                                         \
+                                                                      \
+        /* Compare outputs */                                         \
+        int err = 0;                                                  \
+        for (size_t i = 0; i < X25519_SIZE; i++) {                    \
+            if (out_ref[i] != out_test[i]) {                          \
+                debug_printf("Mismatch at index %zu: ref=%u, test=%u\n", \
+                             i, out_ref[i], out_test[i]);             \
+                err = 1;                                              \
+            }                                                         \
+        }                                                             \
+                                                                      \
+        if (err) {                                                    \
+            debug_printf("Test FAILED for " #func "\n");              \
+            return 1;                                                 \
+        }                                                             \
+        debug_printf("Test PASSED for " #func "\n");                  \
+        return 0;                                                     \
+    }
+#define MAKE_TEST_X25519_single(var, func, ref_func)                  \
+    int test_x25519_##var()                                           \
+    {                                                                 \
+        debug_printf("25519 Correctness Test for " #func);            \
+        uint32_t out_ref[X25519_SIZE] __attribute__((aligned(16))) = {0}; \
+        uint32_t out_test[X25519_SIZE] __attribute__((aligned(16))) = {0}; \
+        ref_func(out_ref);                                            \
+        func(out_test);                                               \
+                                                                      \
+        /* Compare outputs */                                         \
+        int err = 0;                                                  \
+        for (size_t i = 0; i < X25519_SIZE; i++) {                    \
+            if (out_ref[i] != out_test[i]) {                          \
+                debug_printf("Mismatch at index %zu: ref=%u, test=%u\n", \
+                             i, out_ref[i], out_test[i]);             \
+                err = 1;                                              \
+            }                                                         \
+        }                                                             \
+                                                                      \
+        if (err) {                                                    \
+            debug_printf("Test FAILED for " #func "\n");              \
+            return 1;                                                 \
+        }                                                             \
+        debug_printf("Test PASSED for " #func "\n");                  \
+        return 0;                                                     \
+    }
 
-// MAKE_TEST_KECCAKF(pqm4, KeccakF1600_StatePermute_old, KeccakF1600_StatePermute_pqm4)
-// MAKE_TEST_KECCAKF(m7, KeccakP1600_Permute_24rounds, KeccakF1600_StatePermute_pqm4)
-
-// MAKE_TEST_KECCAKF(old_opt_m7, KeccakF1600_StatePermute_old_opt_m7, KeccakF1600_StatePermute_pqm4)
+MAKE_TEST_X25519(fe25519_add, fe25519_add_wrap, fe25519_add_opt_m7_wrap)
+MAKE_TEST_X25519(fe25519_sub, fe25519_sub_wrap, fe25519_sub_opt_m7_wrap)
+MAKE_TEST_X25519(fe25519_mul, fe25519_mul_wrap, fe25519_mul_opt_m7_wrap)
+MAKE_TEST_X25519_single(fe25519_sqr, fe25519_sqr_wrap, fe25519_sqr_opt_m7_wrap)
 
 int main(void)
 {
-    debug_test_start( "====================== x25519 new" );
-
+    debug_test_start("====================== x25519 new");
 
     /* Test correctness */
-    // if(test_keccakf_pqm4() != 0){
-    //     return 1;
-    // }
+    if (test_x25519_fe25519_add() != 0) {
+        debug_printf("add?");
+        return 1;
+    }
+    if (test_x25519_fe25519_sub() != 0) {
+        debug_printf("sub?");
+        return 1;
+    }
+    if (test_x25519_fe25519_mul() != 0) {
+        debug_printf("mul?");
+        return 1;
+    }
+    if (test_x25519_fe25519_sqr() != 0) {
+        debug_printf("sqr?");
+        return 1;
+    }
 
-    // if(test_keccakf_m7()) {
-    //     return 1;
-    // }
 
-    // if(test_keccakf_old_opt_m7()) {
-    //     return 1;
-    // }
-
-    /* Bench */
+//     /* Bench */
     bench_x25519_fe25519_add();
     bench_x25519_fe25519_sub();
     bench_x25519_fe25519_mul();
@@ -138,11 +220,7 @@ int main(void)
     bench_x25519_fe25519_mul_opt_m7();
     bench_x25519_fe25519_sqr_opt_m7();
 
-    //debug_printf("======================" );
-    //dump_benchmarks_tex();
-    debug_printf("======================\n" );
-
-    //debug_test_ok();
-    debug_printf( "ALL GOOD!\n" );
+    debug_printf("======================\n");
+    debug_printf("ALL GOOD!\n");
     return 0;
 }
